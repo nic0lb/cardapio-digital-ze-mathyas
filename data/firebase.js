85a51c3cedcc195c8f7f218bb3e8aa584dc06ac8
nic0lb <126337346+nic0lb@users.noreply.github.com>
2026-03-10 10:07:09 -0300

    Update firebase.js

--- data/firebase.js
+++ data/firebase.js
@@ -1,70 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyAuLVM982MUnK0-8HT0LdpBxhm_cOS-0Os",
   authDomain: "cardapio-digital-845ac.firebaseapp.com",
   projectId: "cardapio-digital-845ac",
   storageBucket: "cardapio-digital-845ac.firebasestorage.app",
   messagingSenderId: "136698316142",
-  appId: "1:136698316142:web:3a193530cb2662b588ed7f",
-  measurementId: "G-ZH5D985G0Y"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAuLVM982MUnK0-8HT0LdpBxhm_cOS-0Os",
-  authDomain: "cardapio-digital-845ac.firebaseapp.com",
-  projectId: "cardapio-digital-845ac",
-  storageBucket: "cardapio-digital-845ac.firebasestorage.app",
-  messagingSenderId: "136698316142",
-  appId: "1:136698316142:web:3a193530cb2662b588ed7f",
-  measurementId: "G-ZH5D985G0Y"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAuLVM982MUnK0-8HT0LdpBxhm_cOS-0Os",
-  authDomain: "cardapio-digital-845ac.firebaseapp.com",
-  projectId: "cardapio-digital-845ac",
-  storageBucket: "cardapio-digital-845ac.firebasestorage.app",
-  messagingSenderId: "136698316142",
-  appId: "1:136698316142:web:3a193530cb2662b588ed7f",
-  measurementId: "G-ZH5D985G0Y"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  appId: "1:136698316142:web:3a193530cb2662b588ed7f"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
